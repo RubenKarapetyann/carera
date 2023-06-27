54619d59c67e383cdaa168668d91498d04b918cc
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-06-27 19:05:34 +0400

    add client constants

---
+++ client/constants/constants.js
@@ -0,0 +1,17 @@
+export const ROUTES = {
+    HOME : "home",
+    VIEW : "view"
+}
+
+export const HEADER_ITEMS = [
+    {
+        displayName : "HOME",
+        id : 1,
+        RouteName : ROUTES.HOME
+    },
+    {
+        displayName : "VIEW",
+        id : 2,
+        RouteName : ROUTES.VIEW
+    }
+]
