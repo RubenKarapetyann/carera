ea569be8f42bc2ea88f27bdf2badd02f3ae0c77b
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-06-28 16:29:51 +0400

    graphic style

--- client/src/components/Home/Graph/Graph.js
+++ client/src/components/Home/Graph/Graph.js
@@ -1,10 +1,14 @@
 import styles from "./Graph.module.css"
+import Graphic from "./components/Graphic"
 
 
 
 function Graph(){
     return(
         <div className={styles["graph-container"]}>
+            <div className={styles["graph-inner-container"]}>
+                <Graphic/>
+            </div>
         </div>
     )
 }
---
+++ client/src/components/Home/Graph/components/Graphic.js
@@ -0,0 +1,12 @@
+import styles from "../Graph.module.css"
+
+
+const Graphic = ()=>{
+    return(
+        <div className={styles["graphic"]}>
+
+        </div>
+    )
+}
+
+export default Graphic
