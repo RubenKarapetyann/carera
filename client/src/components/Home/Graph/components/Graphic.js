6cafa06a2588a393aa7e6b7db1693b74da101b3b
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-06-28 16:53:12 +0400

    create timeLine and countLine images & gives them styles

--- client/src/components/Home/Graph/components/Graphic.js
+++ client/src/components/Home/Graph/components/Graphic.js
@@ -1,10 +1,23 @@
 import styles from "../Graph.module.css"
-
+import countLine from "../../../../images/countLine.svg"
+import timeLine from "../../../../images/timeline.svg"
 
 const Graphic = ()=>{
     return(
         <div className={styles["graphic"]}>
+            <img
+                src={countLine}
+                alt="countLine" 
+                className={styles["count-line"]}
+            />
 
+            
+
+            <img
+                src={timeLine}
+                alt="timeLine" 
+                className={styles["time-line"]}
+            />
         </div>
     )
 }
