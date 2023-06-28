fbbaba168df52abebec71f2ca88040dd15616200
@paytsar.harutyunyan1994 <paytsar.harutyunyan1994@gmail.com>
2023-06-29 02:37:16 +0400

    modified welcome css

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import styles from './Home.module.css'
 import Graph from './Graph/Graph'
+import Welcome from './Welcome/Welcome'
 
 function Home (){
     return (
         <div className={styles["home-container"]}>
+            <Welcome/>
             <Graph/>
         </div>
     )
