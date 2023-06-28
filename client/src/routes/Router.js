30c30e867e8218b3343c30281b8b3b823506dcda
whishad <coderlweer@gmail.com>
2023-06-28 14:38:24 +0400

    change Router code

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -1,12 +1,14 @@
 import { Navigate, Route, Routes } from "react-router"
 import { ROUTES } from "../constants/constants"
+import { lazy } from "react"
 
+const Home = lazy(() => import('../components/Home/Home'))
 
 function Router(){
     return(
         <Routes>
             <Route path="*" element={<Navigate to={ROUTES.HOME}/>}/>
-            <Route path={ROUTES.HOME} element={<p>place here home element</p>}/>
+            <Route path={ROUTES.HOME} element={<Home/>}/>
             <Route path={ROUTES.VIEW} element={<p>place here view element</p>}/>
         </Routes>
     )
