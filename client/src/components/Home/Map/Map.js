fc5e61a7749eff22f652de7830b6414ea9122c35
whishad <coderlweer@gmail.com>
2023-07-01 18:21:19 +0400

    call mapimage in map component

--- client/src/components/Home/Map/Map.js
+++ client/src/components/Home/Map/Map.js
@@ -1,9 +1,11 @@
 import styles from './Map.module.css';
 import Text from '../../Global/Text';
+import MapImage from './components/MapImage';
 
 function Map() {
     return (
         <div className={styles["map-container"]}>
+            <MapImage/>
             <Text textClassName={styles["map-text"]}>
                 our cars in international markets, you can see<br/>
                 <span className={styles["map-text-span1"]}>the current map of the countries in<br/></span>
