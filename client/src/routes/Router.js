204331731cd01f698f6f80916527311b9fa50041
@paytsar.harutyunyan1994 <paytsar.harutyunyan1994@gmail.com>
2023-07-04 13:30:24 +0400

    add view route

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -3,13 +3,14 @@ import { ROUTES } from "../constants/constants"
 import { lazy } from "react"
 
 const Home = lazy(() => import('../components/Home/Home'))
+const View = lazy(() => import('../components/View/View'))
 
 function Router(){
     return(
         <Routes>
             <Route path="*" element={<Navigate to={ROUTES.HOME}/>}/>
             <Route path={ROUTES.HOME} element={<Home/>}/>
-            <Route path={ROUTES.VIEW} element={<p>place here view element</p>}/>
+            <Route path={ROUTES.VIEW} element={<View/>}/>
         </Routes>
     )
 }
