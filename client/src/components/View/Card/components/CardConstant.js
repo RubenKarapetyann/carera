b022c3e3bc05220c2c9e928783fbf3aad76f41ca
@paytsar.harutyunyan1994 <paytsar.harutyunyan1994@gmail.com>
2023-07-04 13:08:47 +0400

    add card constants

---
+++ client/src/components/View/Card/components/CardConstant.js
@@ -0,0 +1,34 @@
+import careraG4 from '../../../../images/CareraG4.png';
+import careraS8 from '../../../../images/CareraS8.png';
+import careraB2 from '../../../../images/CareraB2.png';
+
+export const CARERAG4DATA = {
+    "id": 1,
+    "image": careraG4,
+    "alt": "careraG4",
+    "title": "CARERA",
+    "model": "G4",
+    "description": "Carera G4 is a car from the previous generation, made from a combination of carbon and steel alloys. It is one of our durable models...",
+    "price": "$3,2M"
+}
+
+export const CARERAS8DATA = {
+    "id": 2,
+    "image": careraS8,
+    "alt": "careraS8",
+    "title": "CARERA",
+    "model": "S8",
+    "description": "Carera S8 is the latest model in the family, developed in 2023. A multitude of skilled experts from around the world worked on the design of the car...",
+    "price": "$3,5M"
+}
+  
+export const CARERAB2DATA = {
+    "id": 3,
+    "image": careraB2,
+    "alt": "careraB2",
+    "title": "CARERA",
+    "model": "B2",
+    "description": "Carera B2 is a 2021 model, manufactured in Germany. It is popular in European countries and is known for being one of the fast and agile models...",
+    "price": "$2,9M"
+}
+  
