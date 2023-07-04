1a5dc2cf5d1dceeadd33d9652cc17ef285818d01
@paytsar.harutyunyan1994 <paytsar.harutyunyan1994@gmail.com>
2023-07-04 15:37:15 +0400

    fix view search

--- client/src/components/View/Search/Search.js
+++ client/src/components/View/Search/Search.js
@@ -4,26 +4,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Search.module.css';
 
 function Search() {
-  const [inputSize, setInputSize] = useState({ fontSize: '14px', width: '50px' });
+  const [inputVisible, setInputVisible] = useState(false);
 
   const handleClick = () => {
-    setInputSize({ fontSize: '18px', width: '200px' });
+    setInputVisible(true);
   };
 
   const handleBlur = () => {
-    setInputSize({ fontSize: '14px', width: '50px' });
+    setInputVisible(false);
+  };
+
+  const inputStyle = {
+    width: inputVisible ? '200px' : '50px',
+    fontSize: inputVisible ? '18px' : '14px',
   };
 
   return (
     <div className={styles['search-container']}>
-      <input
-        className={styles['search-input']}
-        type="text"
-        style={inputSize}
-        onClick={handleClick}
-        onBlur={handleBlur}
-      />
-      <FontAwesomeIcon className={styles['search-icon']} icon={faSearch} />
+      {!inputVisible && (
+        <button className={styles['search-button']} style={inputStyle} onClick={handleClick}>
+          <FontAwesomeIcon className={styles['search-icon']} icon={faSearch} />
+        </button>
+      )}
+
+    {inputVisible && (
+      <div className={styles['search-input-container']}>
+        <input
+          className={styles['search-input']}
+          type="text"
+          style={inputStyle}
+          onBlur={handleBlur}
+        />
+        <FontAwesomeIcon className={styles['search-icon-input']} icon={faSearch} />
+      </div>
+    )}
     </div>
   );
 }
