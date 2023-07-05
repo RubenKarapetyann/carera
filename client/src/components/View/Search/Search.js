eaf8b0de98002f5e750caa3ed22505cc3b0e3cde
@paytsar.harutyunyan1994 <paytsar.harutyunyan1994@gmail.com>
2023-07-05 20:16:09 +0400

    clear view search code

--- client/src/components/View/Search/Search.js
+++ client/src/components/View/Search/Search.js
@@ -4,42 +4,48 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Search.module.css';
 
 function Search() {
-  const [inputVisible, setInputVisible] = useState(false);
+  const [inputVisible, setInputVisible] = useState(false)
+
 
   const handleClick = () => {
-    setInputVisible(true);
+    setInputVisible(true)
   };
 
   const handleBlur = () => {
-    setInputVisible(false);
-  };
+    setInputVisible(false)
+  }
+
 
   const inputStyle = {
     width: inputVisible ? '200px' : '50px',
     fontSize: inputVisible ? '18px' : '14px',
-  };
+  }
 
   return (
     <div className={styles['search-container']}>
       {!inputVisible && (
-        <button className={styles['search-button']} style={inputStyle} onClick={handleClick}>
+        <button
+          className={styles['search-button']}
+          style={inputStyle}
+          onClick={handleClick}
+        >
           <FontAwesomeIcon className={styles['search-icon']} icon={faSearch} />
         </button>
       )}
 
-    {inputVisible && (
-      <div className={styles['search-input-container']}>
-        <input
-          className={styles['search-input']}
-          type="text"
-          style={inputStyle}
-          onBlur={handleBlur}
-        />
-        <FontAwesomeIcon className={styles['search-icon-input']} icon={faSearch} />
-      </div>
-    )}
+      {inputVisible && (
+        <div className={styles['search-input-container']}>
+          <input
+            className={styles['search-input']}
+            type="text"
+            style={inputStyle}
+            onBlur={handleBlur}
+          />
+          <FontAwesomeIcon className={styles['search-icon-input']} icon={faSearch} />
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default Search;
+export default Search
