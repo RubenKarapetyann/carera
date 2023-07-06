968f9ff6eb1668e7d08fab33f52fe3b4bf920494
whishad <coderlweer@gmail.com>
2023-07-06 19:17:58 +0400

    write a handleActive function that changes the selected category

--- client/src/components/View/Categories/Categories.js
+++ client/src/components/View/Categories/Categories.js
@@ -3,6 +3,9 @@ import Button from '../../Global/Button';
 import { CATEGORIES } from '../../../constants/constants'
 
 function Categories({state, statefunc}) {
+    const handleActive = (activeName) => {
+      statefunc(activeName)
+    }
     return (
       <div className={styles['categories-container']}>
         {CATEGORIES.map(category => {
@@ -10,7 +13,8 @@ function Categories({state, statefunc}) {
           buttonName={category.name}
           buttonClassName={styles['categories-button']} 
           key = {category.id}
-          styles = {{fontFamily: category.activeName === state ? "Righteous" : ""}}/>
+          styles = {{fontFamily: category.activeName === state ? "Righteous" : ""}}
+          handle={() => handleActive(category.activeName)}/>
         })}
       </div>
     );
