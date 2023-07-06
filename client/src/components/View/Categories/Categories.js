c7b1fe61a247cd008f64c643d1bdd7618fe55a40
whishad <coderlweer@gmail.com>
2023-07-06 18:48:51 +0400

    change categories render way

--- client/src/components/View/Categories/Categories.js
+++ client/src/components/View/Categories/Categories.js
@@ -1,12 +1,16 @@
 import styles from './Categories.module.css'
 import Button from '../../Global/Button';
+import { CATEGORIES } from '../../../constants/constants'
 
 function Categories() {
     return (
       <div className={styles['categories-container']}>
-        <Button buttonName="All Cars" buttonClassName={styles['categories-button']}/>
-        <Button buttonName="Current Year" buttonClassName={styles['categories-button']}/>
-        <Button buttonName="Previous Years" buttonClassName={styles['categories-button']}/>
+        {CATEGORIES.map(category => {
+          return <Button 
+          buttonName={category.name}
+          buttonClassName={styles['categories-button']} 
+          key = {category.id}/>
+        })}
       </div>
     );
   }
