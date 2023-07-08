83ea45cd3313666daf17a190c73e3502aaeb87d9
@paytsar.harutyunyan1994 <paytsar.harutyunyan1994@gmail.com>
2023-07-08 17:41:01 +0400

    write arrowFont code

---
+++ client/src/components/Page3D/components/ArrowFont.js
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import styles from '../Page3D.module.css';
+
+
+const ArrowFont = () => {
+
+    return (
+        <button className={styles['page3D-button']} onClick={backHandle}>
+            <FontAwesomeIcon icon={faArrowLeft} className={styles['page3D-font-icon']}/>
+        </button>
+    )
+}
+
+export default ArrowFont
