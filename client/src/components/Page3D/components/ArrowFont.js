4452d566000f85b97d588bbf77d2d53bc5014bcd
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-08 19:15:34 +0400

    add function for arrow and car color

--- client/src/components/Page3D/components/ArrowFont.js
+++ client/src/components/Page3D/components/ArrowFont.js
@@ -1,10 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import styles from '../Page3D.module.css';
+import { useNavigate } from "react-router"
 
-
-const ArrowFont = ({ backHandle }) => {
-
+const ArrowFont = () => {
+    const navigate = useNavigate()
+    const backHandle = ()=>navigate(-1)
+    
     return (
         <button className={styles['page3D-button']} onClick={backHandle}>
             <FontAwesomeIcon icon={faArrowLeft} className={styles['page3D-font-icon']}/>
@@ -12,4 +14,4 @@ const ArrowFont = ({ backHandle }) => {
     )
 }
 
-export default ArrowFont
+export default ArrowFont;   
--- client/src/components/Page3D/components/CarColor.js
+++ client/src/components/Page3D/components/CarColor.js
@@ -1,10 +1,12 @@
 import styles from '../Page3D.module.css';
 import Button from '../../Global/Button';
 
-const CarColor = ({ colors=[] , colorHandleInner }) => {
+const CarColor = ({ colors , colorHandle }) => {
     return (
         <div className={styles['carColor-container']}>
+            
             {colors && colors.map((color,i)=>{
+                const colorHandleInner = ()=>colorHandle(color)
                 return <Button 
                     handle={colorHandleInner}
                     key={i} 
