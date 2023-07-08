7ff42cd4bde7dec03eb78dd82dee97b4710dd277
@paytsar.harutyunyan1994 <paytsar.harutyunyan1994@gmail.com>
2023-07-08 17:43:24 +0400

    write description code

---
+++ client/src/components/Page3D/components/Description.js
@@ -0,0 +1,17 @@
+import styles from '../Page3D.module.css';
+
+const Description = ({ car }) => {
+    return (
+        <div className={styles['description-container']}>
+            <div className={styles['description-text']}>
+                <p>{car.hp}</p>
+                <p>{car.engineCapacity}</p>
+                <p>{car.engine}</p>
+                <p>{car.engineType}</p>
+                <p>{car.transmission}</p>
+            </div>
+        </div>
+    );
+      } 
+
+export default Description;
