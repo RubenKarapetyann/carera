5a163f10693da36837c02148391747a9649f55b5
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-08 19:04:25 +0400

    add fetchData function

--- client/src/components/Page3D/Page3D.js
+++ client/src/components/Page3D/Page3D.js
@@ -1,21 +1,38 @@
 import { Canvas } from '@react-three/fiber';
 import styles from './Page3D.module.css';
-import ArrowFont from './components/ArrowFont';
-import Description from './components/Description';
-import CarColor from './components/CarColor';
+import { useParams } from 'react-router';
+import { useEffect, useState } from 'react';
+import ArrowFont from './components/ArrowFont'
+import Description from './components/Description'
+import CarColor from './components/CarColor'
 
 function Page3D() {
+    const [data, setData] = useState({});
+    const { id } = useParams()
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+    
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch("/car/"+id);
+            const jsonData = await response.json();
+            setData(jsonData.car);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
     return (
         <div className={styles['page3D-container']}>
-            <ArrowFont/>
-            <Description />
-            
-                <Canvas>
-                </Canvas>
-       
-
-            <CarColor/>
-        </div>
+        <ArrowFont/>
+        <Description car={data}/>
+            <Canvas>
+            </Canvas>
+        <CarColor/>
+    </div>
     )
 }
 
