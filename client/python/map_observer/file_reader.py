9d2477922b3bb0e381a393616c9b6f7df9986e74
whishad <coderlweer@gmail.com>
2023-07-12 13:37:56 +0400

    write find_countries_id function and read MapVector.js

--- client/python/map_observer/file_reader.py
+++ client/python/map_observer/file_reader.py
@@ -1,2 +1,10 @@
 import re
 from data_collector import countries_id
+
+def find_countries_id(id):
+    with open("client/src/components/Home/Map/components/MapVector.js", "r") as file:
+        filetext = file.read()
+
+
+if "__main__" == __name__:
+    find_countries_id(countries_id)
