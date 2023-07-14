93c0e64d601e809a7f27285873ebb97f3250e356
PaytsarHarutyunyan <paytsar.harutyunyan1994@gmail.com>
2023-07-14 12:51:44 +0400

    write Autoservice function and style

---
+++ client/src/components/Autoservice/Autoservice.js
@@ -0,0 +1,10 @@
+import styles from './Autoservice.module.css';
+
+function Autoservice () {
+    return (
+        <div className={styles['autoservice']}>
+        </div>
+    )
+}
+
+export default Autoservice;
