33cda568d96dc10e3cf73e8ae6927bb44cfbae4a
PaytsarHarutyunyan <paytsar.harutyunyan1994@gmail.com>
2023-07-15 13:14:49 +0400

    import logo in autoservice and move in div

--- client/src/components/Autoservice/Autoservice.js
+++ client/src/components/Autoservice/Autoservice.js
@@ -1,11 +1,13 @@
 import styles from './Autoservice.module.css';
 import AutoserviceList from './components/AutoserviceList';
+import Logo from './components/Logo';
 
 function Autoservice () {
     return (
         <div className={styles['autoservice']}>
             <div className={styles['autoservice-menu']}>
                 <AutoserviceList/>
+                <Logo/>
             </div>
         </div>
     )
