a315d370286e4f0d6cdf3f76c65924736598e821
PaytsarHarutyunyan <paytsar.harutyunyan1994@gmail.com>
2023-07-15 12:55:35 +0400

    import autoservicelist in autoservice and move it in div

--- client/src/components/Autoservice/Autoservice.js
+++ client/src/components/Autoservice/Autoservice.js
@@ -1,8 +1,12 @@
 import styles from './Autoservice.module.css';
+import AutoserviceList from './components/AutoserviceList';
 
 function Autoservice () {
     return (
         <div className={styles['autoservice']}>
+            <div className={styles['autoservice-menu']}>
+                <AutoserviceList/>
+            </div>
         </div>
     )
 }
