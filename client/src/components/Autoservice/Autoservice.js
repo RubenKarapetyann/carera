3c4b7dc07ffc09ca2c84b9f33b9a149eda90eecb
whishad <coderlweer@gmail.com>
2023-07-16 14:00:51 +0400

    get id using useparams

--- client/src/components/Autoservice/Autoservice.js
+++ client/src/components/Autoservice/Autoservice.js
@@ -3,9 +3,12 @@ import AutoserviceList from './components/AutoserviceList';
 import Logo from './components/Logo';
 import GoogleMap from './components/GoogleMap';
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
 
 function Autoservice () {
     const [datamap, setMap] = useState(null)
+    const { id } = useParams()
+
     useEffect(() => {
         fetch("/map")
         .then(response => response.json())
