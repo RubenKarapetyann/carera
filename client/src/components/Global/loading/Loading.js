4c0382dc7779fe198ed63986e6a6976788db5a76
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-17 01:23:13 +0400

    loading icon and it style

--- client/src/components/Global/loading/Loading.js
+++ client/src/components/Global/loading/Loading.js
@@ -1,9 +1,11 @@
 import styles from "./Loading.module.css"
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Loading = ()=>{
     return (
         <div className={styles["loading-container"]}>
-            Loading...
+            <span className={styles["loading-icon-container"]}><FontAwesomeIcon icon={faSpinner}/></span>
         </div>
     )
 }
