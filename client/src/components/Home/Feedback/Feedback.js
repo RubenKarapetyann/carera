a2a19677945714c148a8d24e100587b5baeb0f6a
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-20 00:08:51 +0400

    inputs control and feedback active state control

--- client/src/components/Home/Feedback/Feedback.js
+++ client/src/components/Home/Feedback/Feedback.js
@@ -4,17 +4,37 @@ import { useState } from 'react'
 
 function Feedback (){
     const [isActiveFeedback, setIsActiveFeedback] = useState(false)
+    const [inputData,setInputData] = useState({
+        email : "",
+        description : ""
+    })
 
+
+    const feedbackBtnHandle = ()=>setIsActiveFeedback(prev=>!prev)
+    const emailChangeHandle = e =>setInputData(data=>({...data,email : e.target.value}))
+    const descriptionChangeHandle = e =>setInputData(data=>({...data,description : e.target.value}))
     return (
         <div className={styles["feedback-container"]}>
-            <div className={styles["feedback-button"]}>
+            <div className={styles["feedback-button"]} onClick={feedbackBtnHandle}>
                 <span className={styles["feedback-button-text"]}>feedback</span>
             </div>
+            {isActiveFeedback &&
             <form className={styles["feedback-form-container"]}>
-                <input placeholder="yourmailname@gmail.com" className={`${styles["feedback-email-input"]} ${styles["feedback-input"]}`} required type="email"/>
-                <textarea placeholder="write your opinion about the site" className={`${styles["feedback-description-input"]} ${styles["feedback-input"]}`} required/>
+                <input 
+                    placeholder="yourmailname@gmail.com" 
+                    className={`${styles["feedback-email-input"]} ${styles["feedback-input"]}`} 
+                    required 
+                    type="email"
+                    value={inputData.email}
+                    onChange={emailChangeHandle}/>
+                <textarea 
+                    placeholder="write your opinion about the site" 
+                    className={`${styles["feedback-description-input"]} ${styles["feedback-input"]}`} 
+                    required
+                    value={inputData.description}
+                    onChange={descriptionChangeHandle}/>
                 <button className={styles["feedback-submit-button"]}>send</button>
-            </form>
+            </form>}
         </div>
     )
 }
