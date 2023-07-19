6b905f57cc3365e642404db33723208002d9ee73
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-19 23:43:18 +0400

    feedback container and form container

--- client/src/components/Home/Feedback/Feedback.js
+++ client/src/components/Home/Feedback/Feedback.js
@@ -6,8 +6,13 @@ function Feedback (){
     const [isActiveFeedback, setIsActiveFeedback] = useState(false)
 
     return (
-        <div className={styles["feedback-button"]}>
-            <span className={styles["feedback-button-text"]}>feedback</span>
+        <div className={styles["feedback-container"]}>
+            <div className={styles["feedback-button"]}>
+                <span className={styles["feedback-button-text"]}>feedback</span>
+            </div>
+            <div className={styles["feedback-form-container"]}>
+
+            </div>
         </div>
     )
 }
